ded57139d9621a9226d4881e9b400b73dc6ac5d7
Merouane Atig <yahoocbien-dev@yahoo.fr>
2012-09-20 15:31:39 +0200

    create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mealsdo::Application.config.secret_token = 'e6311d6c2189393b1895ec1e2b5a3789f7eea5cd5148e12cfd88992405a135f208457845f7751e7049d2f8f121bf6f06c8a908b6808f169e3dc3fb352b27daf2'
